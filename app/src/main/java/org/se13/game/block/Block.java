505f3edace4447456c05e915b5ac2efc642e330b
someh <someh2705@gmail.com>
2024-03-19 22:43:34 +0900

    feat: 테트리미노 데이터 정의

--- app/src/main/java/org/se13/game/block/Block.java
+++ app/src/main/java/org/se13/game/block/Block.java
@@ -1,18 +1,73 @@
-package org.se13.block;
+package org.se13.game.block;
 
-enum BlockRotationStatus {
+public enum Block {
+    IBlock(new int[][][]{
+            {{1, 0}, {1, 1}, {1, 2}, {1, 3}},
+            {{0, 2}, {1, 2}, {2, 2}, {3, 2}},
+            {{2, 0}, {2, 1}, {2, 2}, {2, 3}},
+            {{0, 1}, {1, 1}, {2, 1}, {3, 1}},
+    }, new int[]{-1, 3}),
 
-}
+    JBlock(new int[][][]{
+            {{0, 0}, {1, 0}, {1, 1}, {1, 2}},
+            {{0, 1}, {0, 2}, {1, 1}, {2, 1}},
+            {{1, 0}, {1, 1}, {1, 2}, {2, 2}},
+            {{0, 1}, {1, 1}, {2, 0}, {2, 1}},
+    }, new int[]{0, 3}),
 
-public abstract class Block {
-    enum BlockRotationStatus {
-        NONE,
+    LBlock(new int[][][]{
+            {{0, 2}, {1, 0}, {1, 1}, {1, 2}},
+            {{0, 1}, {1, 1}, {2, 1}, {2, 2}},
+            {{1, 0}, {1, 1}, {1, 2}, {2, 0}},
+            {{0, 0}, {0, 1}, {1, 1}, {2, 1}},
+    }, new int[]{0, 3}),
 
+    OBlock(new int[][][]{
+            {{0, 0}, {0, 1}, {1, 0}, {1, 1}}
+    }, new int[]{0, 4}),
+
+    SBlock(new int[][][]{
+            {{0, 1}, {0, 2}, {1, 0}, {1, 1}},
+            {{0, 1}, {1, 1}, {1, 2}, {2, 2}},
+            {{1, 1}, {1, 2}, {2, 0}, {2, 1}},
+            {{0, 0}, {1, 0}, {1, 1}, {2, 1}},
+    }, new int[]{0, 3}),
+
+    TBlock(new int[][][]{
+            {{0, 1}, {1, 0}, {1, 1}, {1, 2}},
+            {{0, 1}, {1, 1}, {1, 2}, {2, 1}},
+            {{1, 0}, {1, 1}, {1, 2}, {2, 1}},
+            {{0, 1}, {1, 0}, {1, 1}, {2, 1}},
+    }, new int[]{0, 3}),
+
+    ZBlock(new int[][][]{
+            {{0, 0}, {0, 1}, {1, 1}, {1, 2}},
+            {{0, 2}, {1, 1}, {1, 2}, {2, 1}},
+            {{1, 0}, {1, 1}, {2, 1}, {2, 2}},
+            {{0, 1}, {1, 0}, {1, 1}, {2, 0}},
+    }, new int[]{0, 3});
+
+    Block(int[][][] positions, int[] offset) {
+        int row = positions.length;
+
+        cells = new BlockPosition[row][];
+        startOffset = new BlockPosition(offset[0], offset[1]);
+
+        for (int r = 0; r < row; r++) {
+            int col = positions[r].length;
+            cells[r] = new BlockPosition[col];
+            for (int c = 0; c < col; c++) {
+                cells[r][c] = new BlockPosition(positions[r][c][0], positions[r][c][1]);
+            }
+        }
     }
 
-    protected int blockID;
-    protected BlockPosition[][] cells;
-    protected BlockPosition startBlockPosition;
+    enum BlockRotationStatus {
+        NONE,
+    }
+
+    final BlockPosition[][] cells;
+    final BlockPosition startOffset;
 
     private BlockRotationStatus rotationStatus;
     private BlockPosition position;
