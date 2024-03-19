e55429bd34733a39eba0d2284e0464dce724ffc6
someh <someh2705@gmail.com>
2024-03-19 22:44:53 +0900

    feat: 테트리미노 큐 정의, 7개씩 랜덤하도록 구현

---
+++ app/src/main/java/org/se13/game/rule/BlockQueue.java
@@ -0,0 +1,35 @@
+package org.se13.game.rule;
+
+import org.se13.game.block.Block;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BlockQueue {
+    private final Block[] blocks = new Block[]{
+            Block.IBlock, Block.JBlock, Block.LBlock,
+            Block.OBlock, Block.SBlock, Block.TBlock, Block.ZBlock
+    };
+
+    private final Random random;
+    private final Queue<Block> queue;
+
+    BlockQueue(int seed) {
+        this.random = new Random(seed);
+        this.queue = new ArrayDeque<>();
+    }
+
+    public Block nextBlock() {
+        if (queue.isEmpty()) {
+            List<Block> next = Arrays.asList(blocks);
+            Collections.shuffle(next, random);
+            queue.addAll(next);
+        }
+
+        return queue.poll();
+    }
+}
