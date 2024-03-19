b379732d7a533c2571a767ef9635e0fea43fcb63
binlee0903 <egb3543@gmail.com>
2024-03-19 16:14:18 +0900

    feat: javaFX를 사용하기 위해 main 함수가 포함된 클래스의 모양을 변경

---
+++ app/src/main/java/org/se13/SE13Application.java
@@ -0,0 +1,15 @@
+package org.se13;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SE13Application extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // TODO: javaFX 사용 코드 추가
+    }
+}
