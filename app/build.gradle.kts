8cf7540a0029a32d650e5212be7d2130a793b7d2
binlee0903 <egb3543@gmail.com>
2024-03-21 11:01:09 +0900

    chore: hibernate 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,14 @@ dependencies {
     // https://mvnrepository.com/artifact/org.openjfx/javafx-fxml
     implementation("org.openjfx:javafx-fxml:21.0.2")
 
-    implementation("org.xerial:sqlite-jdbc:3.44.1.0")
+    // https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
+    implementation("org.xerial:sqlite-jdbc:3.45.2.0")
+
+    // https://mvnrepository.com/artifact/org.hibernate.orm/hibernate-core
+    implementation("org.hibernate.orm:hibernate-core:6.4.4.Final")
+
+    // https://mvnrepository.com/artifact/org.hibernate.orm/hibernate-community-dialects
+    implementation("org.hibernate.orm:hibernate-community-dialects:6.4.4.Final")
+
     implementation("org.json:json:20240303")
 }
