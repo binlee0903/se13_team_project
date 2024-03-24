12fe5afeefd26ab04efb1d392d28a671c5bc278c
binlee0903 <egb3543@gmail.com>
2024-03-24 22:56:33 +0900

    fix: 블록 이동이 안되는 버그 해결

--- app/src/main/java/org/se13/game/block/CurrentBlock.java
+++ app/src/main/java/org/se13/game/block/CurrentBlock.java
@@ -16,21 +16,32 @@ public class CurrentBlock {
         return block.shape(rotate);
     }
 
+    public BlockPosition getPosition() {
+        return this.position;
+    }
+
     public void rotateCW() {
-        rotate = (rotate + 1) % block.cells.length;
+        rotate = (rotate + 1) % 4;
     }
 
     public void rotateCCW() {
         if (rotate == 0) {
-            rotate = block.cells.length - 1;
+            rotate = 3;
         } else {
             rotate--;
         }
     }
 
     public void move(int rows, int columns) {
-        position.setRowIndex(rows);
-        position.setColIndex(columns);
+        int newRowIndex = position.getRowPosition() + rows;
+        int newColIndex = position.getColIndex() + columns;
+
+        if (newRowIndex > 9 || newRowIndex < 0 || newColIndex > 21 || newColIndex < 0) {
+            return;
+        }
+
+        position.setRowIndex(newRowIndex);
+        position.setColIndex(newColIndex);
     }
 
     public void reset() {
@@ -39,4 +50,7 @@ public class CurrentBlock {
     }
 
     public int getId() { return block.blockId; }
+    public int getRotateState() {
+        return rotate;
+    }
 }
