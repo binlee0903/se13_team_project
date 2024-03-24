2575a258f7b3dbb99960435d8548740ee5b45181
binlee0903 <egb3543@gmail.com>
2024-03-24 22:55:52 +0900

    feat: 블록 색깔 설정 기능 추가. 단, 나중에 설정에서 가져온 색을 사용하도록 해야함

--- app/src/main/java/org/se13/game/block/Block.java
+++ app/src/main/java/org/se13/game/block/Block.java
@@ -6,53 +6,57 @@ public enum Block {
             {{0, 2}, {1, 2}, {2, 2}, {3, 2}},
             {{2, 0}, {2, 1}, {2, 2}, {2, 3}},
             {{0, 1}, {1, 1}, {2, 1}, {3, 1}},
-    }, new int[]{-1, 3}, 1),
+    }, new int[]{0, 3}, 1, new BlockColor(67, 255, 255)),
 
     JBlock(new int[][][]{
             {{0, 0}, {1, 0}, {1, 1}, {1, 2}},
             {{0, 1}, {0, 2}, {1, 1}, {2, 1}},
             {{1, 0}, {1, 1}, {1, 2}, {2, 2}},
             {{0, 1}, {1, 1}, {2, 0}, {2, 1}},
-    }, new int[]{0, 3}, 2),
+    }, new int[]{0, 3}, 2, new BlockColor(0, 1, 252)),
 
     LBlock(new int[][][]{
             {{0, 2}, {1, 0}, {1, 1}, {1, 2}},
             {{0, 1}, {1, 1}, {2, 1}, {2, 2}},
             {{1, 0}, {1, 1}, {1, 2}, {2, 0}},
             {{0, 0}, {0, 1}, {1, 1}, {2, 1}},
-    }, new int[]{0, 3}, 3),
+    }, new int[]{0, 3}, 3, new BlockColor(254, 165, 0)),
 
     OBlock(new int[][][]{
-            {{0, 0}, {0, 1}, {1, 0}, {1, 1}}
-    }, new int[]{0, 4}, 4),
+            {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
+            {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
+            {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
+            {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
+    }, new int[]{0, 4}, 4, new BlockColor(255, 255, 0)),
 
     SBlock(new int[][][]{
             {{0, 1}, {0, 2}, {1, 0}, {1, 1}},
             {{0, 1}, {1, 1}, {1, 2}, {2, 2}},
             {{1, 1}, {1, 2}, {2, 0}, {2, 1}},
             {{0, 0}, {1, 0}, {1, 1}, {2, 1}},
-    }, new int[]{0, 3}, 5),
+    }, new int[]{0, 3}, 5, new BlockColor(0, 255, 1)),
 
     TBlock(new int[][][]{
             {{0, 1}, {1, 0}, {1, 1}, {1, 2}},
             {{0, 1}, {1, 1}, {1, 2}, {2, 1}},
             {{1, 0}, {1, 1}, {1, 2}, {2, 1}},
             {{0, 1}, {1, 0}, {1, 1}, {2, 1}},
-    }, new int[]{0, 3}, 6),
+    }, new int[]{0, 3}, 6, new BlockColor(170, 0, 255)),
 
     ZBlock(new int[][][]{
             {{0, 0}, {0, 1}, {1, 1}, {1, 2}},
             {{0, 2}, {1, 1}, {1, 2}, {2, 1}},
             {{1, 0}, {1, 1}, {2, 1}, {2, 2}},
             {{0, 1}, {1, 0}, {1, 1}, {2, 0}},
-    }, new int[]{0, 3}, 7);
+    }, new int[]{0, 3}, 7, new BlockColor(254,0, 0));
 
-    Block(int[][][] positions, int[] offset, int id) {
+    Block(int[][][] positions, int[] offset, int id, BlockColor blockColor) {
         int row = positions.length;
 
         blockId = id;
         cells = new BlockPosition[row][];
         startOffset = new BlockPosition(offset[0], offset[1]);
+        this.blockColor = blockColor;
 
         for (int r = 0; r < row; r++) {
             int col = positions[r].length;
@@ -66,6 +70,7 @@ public enum Block {
     public final int blockId;
     public final BlockPosition[][] cells;
     public final BlockPosition startOffset;
+    public final BlockColor blockColor;
 
     public BlockPosition[] shape(int rotate) {
         return cells[rotate % cells.length];
