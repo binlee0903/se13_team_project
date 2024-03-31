dce1877917c3c45f8352c86ae95efa9de256c40f
binlee0903 <egb3543@gmail.com>
2024-03-31 15:13:44 +0900

    feat: 기본 테트리스 게임 구현

--- app/src/main/java/org/se13/game/tetris/DefaultTetrisGame.java
+++ app/src/main/java/org/se13/game/tetris/DefaultTetrisGame.java
@@ -5,6 +5,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import org.se13.game.timer.BlockCollideTimer;
+import org.se13.game.timer.Timer;
 import org.se13.game.block.Block;
 import org.se13.game.block.BlockPosition;
 import org.se13.game.block.CurrentBlock;
@@ -12,10 +14,8 @@ import org.se13.game.config.InputConfig;
 import org.se13.game.grid.TetrisGrid;
 import org.se13.game.input.InputManager;
 import org.se13.game.rule.BlockQueue;
-import org.se13.sqlite.config.ConfigRepository;
 import org.se13.sqlite.config.ConfigRepositoryImpl;
 
-import java.util.Map;
 import java.util.Random;
 
 public class DefaultTetrisGame implements ITetrisGame {
@@ -25,21 +25,30 @@ public class DefaultTetrisGame implements ITetrisGame {
         PAUSED
     }
 
-    public DefaultTetrisGame(Canvas canvas, Label scoreLabel) {
+    public DefaultTetrisGame(Canvas tetrisGameCanvas, Canvas nextBlockCanvas, Label scoreLabel) {
         this.blockQueue = new BlockQueue(new Random().nextInt());
-        this.tetrisGrid = new TetrisGrid(ROW_SIZE, COL_SIZE);
-        this.gc = canvas.getGraphicsContext2D();
-        this.inputManager = InputManager.getInstance(scoreLabel.getScene());
-        this.gameStatus = GameStatus.PAUSED;
+        this.tetrisGameGrid = new TetrisGrid(ROW_SIZE, COL_SIZE);
+        this.gameGraphicsContext = tetrisGameCanvas.getGraphicsContext2D();
+        this.nextBlockGraphicsContext = nextBlockCanvas.getGraphicsContext2D();
         this.scoreLabel = scoreLabel;
+
+        this.gameStatus = GameStatus.PAUSED;
         this.score = 0;
-        this.CANVAS_WIDTH = canvas.getWidth();
-        this.CANVAS_HEIGHT = canvas.getHeight();
+        this.CANVAS_WIDTH = tetrisGameCanvas.getWidth();
+        this.CANVAS_HEIGHT = tetrisGameCanvas.getHeight();
+
+        this.isGameStarted = false;
         this.isBlockPlaced = false;
+        this.isBlockCollided = false;
+
         this.currentBlock = nextBlock();
+        this.nextBlock = nextBlock();
+
         this.configRepository = new ConfigRepositoryImpl();
         this.configRepository.createNewTableConfig();
         this.configRepository.insertDefaultConfig(0);
+
+        this.inputManager = InputManager.getInstance(scoreLabel.getScene());
         this.inputConfig = new InputConfig(this.configRepository);
 
         this.animationTimer = new AnimationTimer() {
@@ -50,6 +59,14 @@ public class DefaultTetrisGame implements ITetrisGame {
                         processUserInput(inputManager.getInput());
                     }
 
+                    if (isGameStarted == false) {
+                        blockMovingTimer = new Timer(l);
+                        collideCheckingTimer = new BlockCollideTimer(l);
+                        isGameStarted = true;
+                        drawNextBlock();
+                    }
+
+                    tick(l);
                     prepare();
                     render();
                     update();
@@ -88,88 +105,112 @@ public class DefaultTetrisGame implements ITetrisGame {
         return new CurrentBlock(blockQueue.nextBlock());
     }
 
-    private boolean blockFits(int rowMovementAmount, int colMovementAmount, int newRotationState) {
-        /*for (BlockPosition p : currentBlock.shape()) {
-            if (!tetrisGrid.isEmptyCell(p.getRowPosition(), p.getColIndex())) {
+    private boolean blockFits() {
+        for (BlockPosition p : currentBlock.shape()) {
+            int blockRowIndex = p.getRowIndex() + currentBlock.getPosition().getRowIndex();
+            int blockColIndex = p.getColIndex() + currentBlock.getPosition().getColIndex();
+
+            if (tetrisGameGrid.isEmptyCell(blockRowIndex, blockColIndex) == false) {
                 return false;
             }
-        }*/
+
+            if (tetrisGameGrid.isInsideGrid(blockRowIndex, blockColIndex) == false) {
+                return false;
+            }
+        }
 
         return true;
     }
 
     private void rotateBlockCW() {
-        int newRotateState = (currentBlock.getRotateState() + 1) % 4;
+        deleteCurrentBlockFromGrid();
+        currentBlock.rotateCW();
 
-        if (blockFits(0, 0, newRotateState)) {
-            deleteBlockFromGrid();
-            currentBlock.rotateCW();
-        }
-    }
-
-    private void rotateBlockCCW() {
-        int newRotateState = currentBlock.getRotateState();
-
-        if (newRotateState == 0) {
-            newRotateState = 3;
-        } else {
-            newRotateState++;
-        }
-
-        if (blockFits(0, 0, newRotateState)) {
-            deleteBlockFromGrid();
+        if (blockFits() == false) {
             currentBlock.rotateCCW();
         }
     }
 
+    private void rotateBlockCCW() {
+        deleteCurrentBlockFromGrid();
+        currentBlock.rotateCCW();
+
+        if (blockFits() == false) {
+            currentBlock.rotateCW();
+        }
+    }
+
     private void moveBlockLeft() {
-        if (blockFits(0, -1, 0)) {
-            deleteBlockFromGrid();
-            currentBlock.move(-1, 0);
-        }
-    }
+        deleteCurrentBlockFromGrid();
+        currentBlock.move(0, -1);
 
-    private void moveBlockRight() {
-        if (blockFits(0, 1, 0)) {
-            deleteBlockFromGrid();
-            currentBlock.move(1, 0);
-        }
-    }
-
-    private void moveBlockDown() {
-        if (blockFits(1, 0, 0)) {
-            deleteBlockFromGrid();
+        if (blockFits() == false) {
             currentBlock.move(0, 1);
         }
     }
 
+    private void moveBlockRight() {
+        deleteCurrentBlockFromGrid();
+        currentBlock.move(0, 1);
+
+        if (blockFits() == false) {
+            currentBlock.move(0, -1);
+        }
+    }
+
+    private void moveBlockDown() {
+        deleteCurrentBlockFromGrid();
+        currentBlock.move(1, 0);
+        this.score++;
+
+        if (blockFits() == false) {
+            isBlockCollided = true;
+            this.score--;
+            currentBlock.move(-1, 0);
+        } else {
+            isBlockCollided = false;
+        }
+    }
+
+    private void immediateBlockPlace() {
+        deleteCurrentBlockFromGrid();
+
+        for (int i = currentBlock.getPosition().getRowIndex(); i < ROW_SIZE; i++) {
+            currentBlock.move(1, 0);
+            if (blockFits() == false) {
+                isBlockPlaced = true;
+                currentBlock.move(-1, 0);
+            }
+        }
+    }
+
     private boolean isGameOver() {
-        return !(tetrisGrid.isRowEmpty(1) && tetrisGrid.isRowEmpty(2));
+        if (tetrisGameGrid.isRowEmpty(0) == true) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     private void drawBlockIntoGrid() {
         BlockPosition currentBlockPosition = currentBlock.getPosition();
 
         for (BlockPosition p : currentBlock.shape()) {
-            tetrisGrid.setCell(p.getRowPosition() + currentBlockPosition.getRowPosition(), p.getColIndex() + currentBlockPosition.getColIndex(), currentBlock.getId());
+            tetrisGameGrid.setCell(p.getRowIndex() + currentBlockPosition.getRowIndex(), p.getColIndex() + currentBlockPosition.getColIndex(), currentBlock.getId());
         }
-
-        /*if (isGameOver() == true) {
-            this.gameStatus = GameStatus.GAMEOVER;
-        }*/
     }
 
-    private void deleteBlockFromGrid() {
+    private void deleteCurrentBlockFromGrid() {
         BlockPosition currentBlockPosition = currentBlock.getPosition();
 
         for (BlockPosition p : currentBlock.shape()) {
-            tetrisGrid.setCell(p.getRowPosition() + currentBlockPosition.getRowPosition(), p.getColIndex() + currentBlockPosition.getColIndex(), 0);
+            tetrisGameGrid.setCell(p.getRowIndex() + currentBlockPosition.getRowIndex(), p.getColIndex() + currentBlockPosition.getColIndex(), 0);
         }
     }
 
     private void processUserInput(char keyCode) {
         if (keyCode == this.inputConfig.UP) {
-
+            immediateBlockPlace();
         } else if (keyCode == this.inputConfig.DOWN) {
             moveBlockDown();
         } else if (keyCode == this.inputConfig.LEFT) {
@@ -187,43 +228,89 @@ public class DefaultTetrisGame implements ITetrisGame {
         }
     }
 
+    private void drawNextBlock() {
+        nextBlockGraphicsContext.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        BlockPosition[] nextBlockPositions = nextBlock.shape();
+
+        int colIndex = 0;
+        int rowIndex = 0;
+
+        for (int i = 0; i < 4; i++) {
+            colIndex = nextBlockPositions[i].getColIndex();
+            rowIndex = nextBlockPositions[i].getRowIndex() + 1; // 더 잘보이게 하기 위해 행 인덱스에 1을 더해줌
+
+            switch (nextBlock.getId()) {
+                case 1: // I Block
+                    nextBlockGraphicsContext.setFill(Block.IBlock.blockColor.getBlockColor());
+                    nextBlockGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), colIndex * TEXT_INTERVAL, rowIndex * TEXT_INTERVAL);
+                    break;
+                case 2: // J Block
+                    nextBlockGraphicsContext.setFill(Block.JBlock.blockColor.getBlockColor());
+                    nextBlockGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), colIndex * TEXT_INTERVAL, rowIndex * TEXT_INTERVAL);
+                    break;
+                case 3: // L Block
+                    nextBlockGraphicsContext.setFill(Block.LBlock.blockColor.getBlockColor());
+                    nextBlockGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), colIndex * TEXT_INTERVAL, rowIndex * TEXT_INTERVAL);
+                    break;
+                case 4: // O Block
+                    nextBlockGraphicsContext.setFill(Block.OBlock.blockColor.getBlockColor());
+                    nextBlockGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), colIndex * TEXT_INTERVAL, rowIndex * TEXT_INTERVAL);
+                    break;
+                case 5: // S Block
+                    nextBlockGraphicsContext.setFill(Block.SBlock.blockColor.getBlockColor());
+                    nextBlockGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), colIndex * TEXT_INTERVAL, rowIndex * TEXT_INTERVAL);
+                    break;
+                case 6: // T Block
+                    nextBlockGraphicsContext.setFill(Block.TBlock.blockColor.getBlockColor());
+                    nextBlockGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), colIndex * TEXT_INTERVAL, rowIndex * TEXT_INTERVAL);
+                    break;
+                case 7:
+                    nextBlockGraphicsContext.setFill(Block.ZBlock.blockColor.getBlockColor());
+                    nextBlockGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), colIndex * TEXT_INTERVAL, rowIndex * TEXT_INTERVAL);
+                    break;
+                default:
+                    nextBlockGraphicsContext.fillText(String.valueOf(' '), colIndex * TEXT_INTERVAL, rowIndex * TEXT_INTERVAL);
+            }
+        }
+    }
+
     private void render() {
-        gc.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        gameGraphicsContext.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
         for (int i = 0; i < ROW_SIZE; i++) {
             for (int j = 0; j < COL_SIZE; j++) {
-                switch (tetrisGrid.getCell(i, j)) {
+                switch (tetrisGameGrid.getCell(i, j)) {
                     case 1: // I Block
-                        gc.setFill(Block.IBlock.blockColor.getBlockColor());
-                        gc.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), i * TEXT_INTERVAL, j * TEXT_INTERVAL);
+                        gameGraphicsContext.setFill(Block.IBlock.blockColor.getBlockColor());
+                        gameGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), j * TEXT_INTERVAL, i * TEXT_INTERVAL);
                         break;
                     case 2: // J Block
-                        gc.setFill(Block.JBlock.blockColor.getBlockColor());
-                        gc.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), i * TEXT_INTERVAL, j * TEXT_INTERVAL);
+                        gameGraphicsContext.setFill(Block.JBlock.blockColor.getBlockColor());
+                        gameGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), j * TEXT_INTERVAL, i * TEXT_INTERVAL);
                         break;
                     case 3: // L Block
-                        gc.setFill(Block.LBlock.blockColor.getBlockColor());
-                        gc.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), i * TEXT_INTERVAL, j * TEXT_INTERVAL);
+                        gameGraphicsContext.setFill(Block.LBlock.blockColor.getBlockColor());
+                        gameGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), j * TEXT_INTERVAL, i * TEXT_INTERVAL);
                         break;
                     case 4: // O Block
-                        gc.setFill(Block.OBlock.blockColor.getBlockColor());
-                        gc.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), i * TEXT_INTERVAL, j * TEXT_INTERVAL);
+                        gameGraphicsContext.setFill(Block.OBlock.blockColor.getBlockColor());
+                        gameGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), j * TEXT_INTERVAL, i * TEXT_INTERVAL);
                         break;
                     case 5: // S Block
-                        gc.setFill(Block.SBlock.blockColor.getBlockColor());
-                        gc.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), i * TEXT_INTERVAL, j * TEXT_INTERVAL);
+                        gameGraphicsContext.setFill(Block.SBlock.blockColor.getBlockColor());
+                        gameGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), j * TEXT_INTERVAL, i * TEXT_INTERVAL);
                         break;
                     case 6: // T Block
-                        gc.setFill(Block.TBlock.blockColor.getBlockColor());
-                        gc.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), i * TEXT_INTERVAL, j * TEXT_INTERVAL);
+                        gameGraphicsContext.setFill(Block.TBlock.blockColor.getBlockColor());
+                        gameGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), j * TEXT_INTERVAL, i * TEXT_INTERVAL);
                         break;
                     case 7:
-                        gc.setFill(Block.ZBlock.blockColor.getBlockColor());
-                        gc.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), i * TEXT_INTERVAL, j * TEXT_INTERVAL);
+                        gameGraphicsContext.setFill(Block.ZBlock.blockColor.getBlockColor());
+                        gameGraphicsContext.fillText(String.valueOf(DEFAULT_BLOCK_TEXT), j * TEXT_INTERVAL, i * TEXT_INTERVAL);
                         break;
                     default:
-                        gc.setFill(Color.WHITE);
-                        gc.fillText(String.valueOf(' '), i * TEXT_INTERVAL, j * TEXT_INTERVAL);
+                        gameGraphicsContext.fillText(String.valueOf(' '), j * TEXT_INTERVAL, i * TEXT_INTERVAL);
                 }
             }
         }
@@ -231,27 +318,58 @@ public class DefaultTetrisGame implements ITetrisGame {
 
     private void update() {
         drawBlockIntoGrid();
-        int clearedRows = tetrisGrid.clearFullRows();
-
-        if (clearedRows > 0) {
-            score += 10 * clearedRows;
-        }
 
         scoreLabel.setText(String.valueOf(score));
 
         if (isBlockPlaced == true) {
-            currentBlock = nextBlock();
+            int clearedRows = tetrisGameGrid.clearFullRows();
+
+            if (clearedRows > 0) {
+                score += 10 * clearedRows;
+            }
+
+            currentBlock = nextBlock;
+            nextBlock = nextBlock();
+            drawNextBlock();
             isBlockPlaced = false;
+
+            if (isGameOver() == true) {
+                gameStatus = GameStatus.GAMEOVER;
+            }
         }
     }
 
     private void prepare() {
-        gc.setFill(new Color(0, 0, 0, 1.0));
-        gc.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        gameGraphicsContext.setFill(new Color(0, 0, 0, 1.0));
+        gameGraphicsContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
 
-    private final int ROW_SIZE = 10;
-    private final int COL_SIZE = 22;
+    private void tick(long l) {
+        blockMovingTimer.setCurrentTime(l);
+        collideCheckingTimer.setCurrentTime(l);
+
+        if ((float) blockMovingTimer.getElapsedTime() / 1000000000 > 1.0f) {
+            moveBlockDown();
+            blockMovingTimer.reset(l);
+        }
+
+        if (isBlockCollided == true) {
+            if (collideCheckingTimer.isBlockPlaceTimeEnded() == true) {
+                isBlockPlaced = true;
+                isBlockCollided = false;
+                collideCheckingTimer.reset(l);
+            }
+
+            if (collideCheckingTimer.isTimerStarted() == false) {
+                collideCheckingTimer.setFirstBlockCollideTime(l);
+            }
+        } else {
+            collideCheckingTimer.reset(l);
+        }
+    }
+
+    private final int ROW_SIZE = 22;
+    private final int COL_SIZE = 10;
     private final int TEXT_INTERVAL = 10;
     private final double CANVAS_WIDTH;
     private final double CANVAS_HEIGHT;
@@ -260,14 +378,18 @@ public class DefaultTetrisGame implements ITetrisGame {
     private ConfigRepositoryImpl configRepository;
     private InputManager inputManager;
     private InputConfig inputConfig;
-    private TetrisGrid tetrisGrid;
+    private TetrisGrid tetrisGameGrid;
     private final BlockQueue blockQueue;
     private CurrentBlock currentBlock;
-    private GraphicsContext gc;
+    private CurrentBlock nextBlock;
+    private final GraphicsContext gameGraphicsContext;
+    private final GraphicsContext nextBlockGraphicsContext;
     private GameStatus gameStatus;
     private Label scoreLabel;
+    private Timer blockMovingTimer;
+    private BlockCollideTimer collideCheckingTimer;
     private int score;
+    private boolean isGameStarted;
     private boolean isBlockPlaced;
-    private long elapsedTime;
-    private long lastTime;
+    private boolean isBlockCollided;
 }
