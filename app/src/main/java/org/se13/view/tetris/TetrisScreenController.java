28e3d4e85f05b0da0d1ab1e5b9da2e51c3c0246c
binlee0903 <egb3543@gmail.com>
2024-03-31 15:37:19 +0900

    refactor: 테트리스 게임 컨트롤러가 Lifecycle을 따르도록 함

--- app/src/main/java/org/se13/view/tetris/TetrisScreenController.java
+++ app/src/main/java/org/se13/view/tetris/TetrisScreenController.java
@@ -1,29 +1,34 @@
 package org.se13.view.tetris;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import org.se13.SE13Application;
+import org.se13.game.tetris.DefaultTetrisGame;
 import org.se13.view.base.BaseController;
 import org.se13.view.nav.Screen;
 
 public class TetrisScreenController extends BaseController {
-
-    @FXML
-    private Button startButton;
-    @FXML
-    private Button settingsButton;
-    @FXML
-    private Button scoreButton;
-    @FXML
-    private Button quitButton;
-
-    @FXML
-    private void handleScoreButtonAction() {
-        SE13Application.navController.navigate(Screen.RANKING);
+    @Override
+    public void onCreate() {
+        this.tetrisGame = new DefaultTetrisGame(gameCanvas, nextBlockCanvas, score);
+        this.frame.setStyle("-fx-border-color: red;");
+    }
+    @Override
+    public void onStart() {
+        this.tetrisGame.startGame();
     }
 
     @FXML
-    private void handleBackButtonAction() {
-        SE13Application.navController.popBackStack();
-    }
+    private Canvas nextBlockCanvas;
+    @FXML
+    private Label score;
+    @FXML
+    private BorderPane frame;
+    @FXML
+    private Canvas gameCanvas;
+
+    private DefaultTetrisGame tetrisGame;
 }
