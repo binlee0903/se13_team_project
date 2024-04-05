5a9a5f6c5415b94d8a0e822364dcfa1f338f418d
Gyeoung Bin Lee <44839537+binlee0903@users.noreply.github.com>
2024-04-05 16:39:46 +0900

    fix: 바뀌지 않은 인자 이름 변경

--- app/src/main/java/org/se13/sqlite/config/ConfigRepository.java
+++ app/src/main/java/org/se13/sqlite/config/ConfigRepository.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public interface ConfigRepository {
     void insertDefaultConfig(int id);
-    void updateConfig(int id, String mode, int gridWidth, int gridHeight, int keyLeft, int keyRight, int keyDown, int keyRotateLeft, int keyRotateRight, int keyPause, int keyHold, int keyExit);
+    void updateConfig(int id, String mode, int gridWidth, int gridHeight, int keyLeft, int keyRight, int keyDown, int keyRotateLeft, int keyRotateRight, int keyPause, int keyDrop, int keyExit);
     Map<String, Object> getConfig(int id);
     void clearConfig(int id);
 }
