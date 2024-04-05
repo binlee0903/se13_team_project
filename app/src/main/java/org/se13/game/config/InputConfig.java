a5e5e350b11410a1f3a6e821912b0c83da825ce9
binlee0903 <egb3543@gmail.com>
2024-04-05 23:33:51 +0900

    fix: 추가된 키 설정을 반영함.

--- app/src/main/java/org/se13/game/config/InputConfig.java
+++ app/src/main/java/org/se13/game/config/InputConfig.java
@@ -1,31 +1,30 @@
 package org.se13.game.config;
 
-import org.se13.game.tetris.DefaultTetrisGame;
 import org.se13.sqlite.config.ConfigRepository;
+import org.se13.sqlite.config.ConfigRepositoryImpl;
 
 import java.util.Map;
 
 public class InputConfig {
-    public InputConfig(ConfigRepository configRepository) {
-        Map<String, Object> config = configRepository.getConfig(0);
-        UP = 'w';
-        DOWN = (char) ((Integer)config.get("keyDown")).intValue();
-        LEFT = (char) ((Integer)config.get("keyLeft")).intValue();
-        RIGHT = (char) ((Integer)config.get("keyRight")).intValue();
-        CCW_SPIN = (char) ((Integer)config.get("keyRotateLeft")).intValue();
-        CW_SPIN = (char) ((Integer)config.get("keyRotateRight")).intValue();
-        PAUSE = (char) ((Integer)config.get("keyPause")).intValue();
-        HOLD = 'h';
-        EXIT = 'q';
+    public InputConfig() {
+        ConfigRepositoryImpl configRepository = ConfigRepositoryImpl.getInstance();
+        Map<String, Object> configs = configRepository.getConfig(0);
+        DROP = (char) ((Integer)configs.get("keyDrop")).intValue();
+        DOWN = (char) ((Integer)configs.get("keyDown")).intValue();
+        LEFT = (char) ((Integer)configs.get("keyLeft")).intValue();
+        RIGHT = (char) ((Integer)configs.get("keyRight")).intValue();
+        CCW_SPIN = (char) ((Integer)configs.get("keyRotateLeft")).intValue();
+        CW_SPIN = (char) ((Integer)configs.get("keyRotateRight")).intValue();
+        PAUSE = (char) ((Integer)configs.get("keyPause")).intValue();
+        EXIT = (char) ((Integer)configs.get("keyExit")).intValue();
     }
 
-    public char UP;
+    public char DROP;
     public char DOWN;
     public char LEFT;
     public char RIGHT;
     public char CW_SPIN;
     public char CCW_SPIN;
     public char PAUSE;
-    public char HOLD;
     public char EXIT;
 }
