41e76b28d446cd235ea342230d91c3e694f4cb88
hyotaime <hyotaime@hyotaime.com>
2024-04-06 13:16:15 +0900

    fix: GameOver2RankingScreen 값 이동 수정 - GameOver에서 닉네임 입력 과정 삭제 - navController.navigate을 이용해 화면 및 값 이동

--- app/src/main/java/org/se13/view/tetris/GameOverScreenController.java
+++ app/src/main/java/org/se13/view/tetris/GameOverScreenController.java
@@ -1,11 +1,12 @@
 package org.se13.view.tetris;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import org.se13.SE13Application;
 import org.se13.game.tetris.DefaultTetrisGame;
 import org.se13.sqlite.ranking.RankingRepositoryImpl;
+import org.se13.view.ranking.RankingScreenController;
 import org.se13.view.base.BaseController;
 import org.se13.view.nav.Screen;
 
@@ -20,10 +21,11 @@ public class GameOverScreenController extends BaseController {
         rankingRepository.createNewTableRanking();
     }
 
-    public void onEnter() {
-        rankingRepository.insertRanking(name.getText(), defaultTetrisGame.getScore(), defaultTetrisGame.isItemMode(), defaultTetrisGame.getDifficulty());
+    public void handleRankingButtonAction() {
+        SE13Application.navController.navigate(Screen.RANKING, (RankingScreenController controller) -> {
+            controller.setArguments(defaultTetrisGame.getScore(), defaultTetrisGame.isItemMode(), defaultTetrisGame.getDifficulty());
+        });
         defaultTetrisGame.resetGame();
-        SE13Application.navController.navigate(Screen.RANKING);
     }
 
     private DefaultTetrisGame defaultTetrisGame;
@@ -31,7 +33,6 @@ public class GameOverScreenController extends BaseController {
 
     @FXML
     private Text score;
-
     @FXML
-    private TextField name;
+    public Button rankingButton;
 }
