2debf30951ea9f0ad0e437119016fcb60dc5a3b2
STkangyh <k22101149@g.seoultech.ac.kr>
2024-04-07 14:04:53 +0900

    fix: key의 아스키코드를 넘기지 못하는 현상 수정

--- app/src/main/java/org/se13/view/setting/SettingScreenController.java
+++ app/src/main/java/org/se13/view/setting/SettingScreenController.java
@@ -11,9 +11,6 @@ import javafx.scene.input.KeyEvent;
 import org.se13.SE13Application;
 import org.se13.sqlite.config.ConfigRepositoryImpl;
 import org.se13.view.base.BaseController;
-
-import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Map;
 
 public class SettingScreenController extends BaseController {
@@ -29,17 +26,17 @@ public class SettingScreenController extends BaseController {
     private ChoiceBox<String> screenSizeChoiceBox;
     @FXML
     private ChoiceBox<String> screenColorBlindChoiceBox;
-    private String selectedScreenSize;
 
     @FXML
     private void initialize() {
         ConfigRepositoryImpl configRepository = ConfigRepositoryImpl.getInstance();
         Map<String, Object> configs = configRepository.getConfig(0);
+        System.out.println(configs);
 
         // Add options in ChoiceBox for the choice among scene size
         int screenWidth = (Integer) configs.get("screenWidth");
         int screenHeight = (Integer) configs.get("screenHeight");
-        selectedScreenSize = screenWidth + "x" + screenHeight;
+        String selectedScreenSize = screenWidth + "x" + screenHeight;
         screenSizeChoiceBox.setItems(FXCollections.observableArrayList("300x400", "600x800", "1920x1080"));
         screenSizeChoiceBox.setValue(selectedScreenSize);
         // Add options in ChoiceBox for the choice among color mode
@@ -48,41 +45,66 @@ public class SettingScreenController extends BaseController {
         screenColorBlindChoiceBox.setValue(colorMode);
         // Add options in buttons for the choice in the keyboard
         String keyMoveLeft = String.valueOf(configs.get("keyLeft"));
-        moveLeftButton.setText("Left: " + asciiCasting(keyMoveLeft));
+        System.out.println(keyMoveLeft);
+        moveLeftButton.setText("Left: " + asciiToString(keyMoveLeft));
 
         String keyMoveRight = String.valueOf(configs.get("keyRight"));
-        moveRightButton.setText("Right: " + asciiCasting(keyMoveRight));
+        moveRightButton.setText("Right: " + asciiToString(keyMoveRight));
 
         String keyMoveDown = String.valueOf(configs.get("keyDown"));
-        moveDownButton.setText("Down: " + asciiCasting(keyMoveDown));
+        moveDownButton.setText("Down: " + asciiToString(keyMoveDown));
 
         String keyExit = String.valueOf(configs.get("keyExit"));
-        exitButton.setText("Exit: " + asciiCasting(keyExit));
+        exitButton.setText("Exit: " + asciiToString(keyExit));
 
         String keyDrop = String.valueOf(configs.get("keyDrop"));
-        moveDropButton.setText("Drop: " + asciiCasting(keyDrop));
+        moveDropButton.setText("Drop: " + asciiToString(keyDrop));
 
         String keyMoveRotate = String.valueOf(configs.get("keyRotateRight"));
-        rotateButton.setText("Rotate: " + asciiCasting(keyMoveRotate));
+        rotateButton.setText("Rotate: " + asciiToString(keyMoveRotate));
 
         String keyPause = String.valueOf(configs.get("keyPause"));
-        pauseButton.setText("Pause: " + asciiCasting(keyPause));
+        pauseButton.setText("Pause: " + asciiToString(keyPause));
         // By selected scene size, the function will implement logic.
 
     }
 
-    private String asciiCasting(String asciiCodeCasting) {
-        int asciiValue = Integer.parseInt(asciiCodeCasting);
-        char asciiChar = (char) asciiValue;
-        return String.valueOf(asciiChar);
+    public String asciiToString(String asciiCode) {
+        try {
+            // 문자열을 정수로 변환
+            int code = Integer.parseInt(asciiCode);
+
+            // 정수 값을 문자로 캐스팅
+            char character = (char) code;
+
+            // 문자를 문자열로 변환하여 반환
+            return String.valueOf(character);
+        } catch (NumberFormatException e) {
+            // 입력 문자열이 유효한 정수가 아닌 경우 오류 처리
+            System.err.println("Invalid ASCII code: " + asciiCode);
+            return null;
+        }
     }
+
+    public int getAsciiCodeFromButtonText(Button button) {
+        // 버튼의 텍스트를 가져옵니다.
+        String text = button.getText();
+
+        // 텍스트가 비어있지 않은 경우 첫 번째 문자의 아스키 코드를 반환합니다.
+        if (!text.isEmpty()) {
+            return text.charAt(text.length()-1); // 첫 번째 문자의 아스키 코드 값을 반환
+        }
+
+        // 텍스트가 비어있는 경우, 오류 코드나 기본 값으로 처리할 수 있습니다.
+        // 예를 들어, 여기서는 -1을 반환합니다.
+        return -1;
+    }
+
 
     @FXML
     private void handleBackButtonAction() {
         // Turn into last scene
         SE13Application.navController.popBackStack();
-        ConfigRepositoryImpl configRepository = ConfigRepositoryImpl.getInstance();
-        configRepository.insertDefaultConfig(0);
     }
 
     @FXML
@@ -100,6 +122,7 @@ public class SettingScreenController extends BaseController {
                     String keyName = keyCode.getName(); // 사용자에게 익숙한 키 이름 가져오기
                     keyName = keyName.toLowerCase();
                     buttonToConfigure.setText(keyName);
+                    System.out.println(keyName);
 
                     // 설정 완료 후 이벤트 리스너 제거
                     buttonToConfigure.getScene().removeEventHandler(KeyEvent.KEY_PRESSED, this);
@@ -114,7 +137,7 @@ public class SettingScreenController extends BaseController {
     }
 
     @FXML
-    private void handleSaveButtonAction() {
+    private void handleSaveButtonAction(ActionEvent event) {
         // Saving personal settings
         // screen size setting
         String selectedSize = screenSizeChoiceBox.getValue();
@@ -124,9 +147,24 @@ public class SettingScreenController extends BaseController {
         // box color setting
         String selectedColorMode = screenColorBlindChoiceBox.getValue();
         // button setting
-        int selectedMoveLeft = Integer.parseInt(moveLeftButton.getText());
+        int selectedMoveLeft = getAsciiCodeFromButtonText(moveLeftButton);
+        int selectedMoveRight = getAsciiCodeFromButtonText(moveRightButton);
+        int selectedMoveDown = getAsciiCodeFromButtonText(moveDownButton);
+        int selectedRotate = getAsciiCodeFromButtonText(rotateButton);
+        int selectedPause = getAsciiCodeFromButtonText(pauseButton);
+        int selectedDrop = getAsciiCodeFromButtonText(moveDropButton);
+        int selectedExit = getAsciiCodeFromButtonText(exitButton);
 
         ConfigRepositoryImpl configRepository = ConfigRepositoryImpl.getInstance();
+        configRepository.updateConfig(0, selectedColorMode, selectedWidth, selectedHeight, selectedMoveLeft, selectedMoveRight, selectedMoveDown, 120, selectedRotate, selectedPause, selectedDrop, selectedExit);
+        System.out.println(selectedDrop);
+    }
+
+    public void handleSettingClearButtonAction() {
+
+    }
+
+    public void handleRankingClearButtonAction() {
 
     }
 }
