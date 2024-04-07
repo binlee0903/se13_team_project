d1d166bdad332314b597b068a15e192f346c1a2c
STkangyh <k22101149@g.seoultech.ac.kr>
2024-04-07 16:35:29 +0900

    feat: 색맹 모드 적용

--- app/src/main/java/org/se13/game/block/Block.java
+++ app/src/main/java/org/se13/game/block/Block.java
@@ -1,54 +1,88 @@
 package org.se13.game.block;
 
+import javafx.scene.paint.Color;
+import org.se13.sqlite.config.ConfigRepositoryImpl;
+
+import java.util.Map;
+import java.util.Objects;
+
 public enum Block {
     IBlock(new int[][][]{
             {{1, 0}, {1, 1}, {1, 2}, {1, 3}},
             {{0, 2}, {1, 2}, {2, 2}, {3, 2}},
             {{2, 0}, {2, 1}, {2, 2}, {2, 3}},
             {{0, 1}, {1, 1}, {2, 1}, {3, 1}},
-    }, new int[]{0, 3}, 1, new BlockColor(67, 255, 255)),
+    }, new int[]{0, 3}, 1, new BlockColor (
+            Color.rgb(0, 0, 255),
+            Color.rgb(0, 0, 255),
+            Color.rgb(255, 0, 0))
+    ),
 
     JBlock(new int[][][]{
             {{0, 0}, {1, 0}, {1, 1}, {1, 2}},
             {{0, 1}, {0, 2}, {1, 1}, {2, 1}},
             {{1, 0}, {1, 1}, {1, 2}, {2, 2}},
             {{0, 1}, {1, 1}, {2, 0}, {2, 1}},
-    }, new int[]{0, 3}, 2, new BlockColor(0, 1, 252)),
+    }, new int[]{0, 3}, 2, new BlockColor(
+            Color.rgb(255, 0, 0),
+            Color.rgb(255, 192, 203),
+            Color.rgb(0, 255, 0))
+    ),
 
     LBlock(new int[][][]{
             {{0, 2}, {1, 0}, {1, 1}, {1, 2}},
             {{0, 1}, {1, 1}, {2, 1}, {2, 2}},
             {{1, 0}, {1, 1}, {1, 2}, {2, 0}},
             {{0, 0}, {0, 1}, {1, 1}, {2, 1}},
-    }, new int[]{0, 3}, 3, new BlockColor(254, 165, 0)),
+    }, new int[]{0, 3}, 3, new BlockColor(
+            Color.rgb(0, 255, 0),
+            Color.rgb(0, 128, 128),
+            Color.rgb(128, 0, 128))
+    ),
 
     OBlock(new int[][][]{
             {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
             {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
             {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
             {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
-    }, new int[]{0, 3}, 4, new BlockColor(255, 255, 0)),
+    }, new int[]{0, 3}, 4, new BlockColor(
+            Color.rgb(255, 255, 0),
+            Color.rgb(255, 255, 0),
+            Color.rgb(135, 206, 235))
+    ),
 
     SBlock(new int[][][]{
             {{0, 1}, {0, 2}, {1, 0}, {1, 1}},
             {{0, 1}, {1, 1}, {1, 2}, {2, 2}},
             {{1, 1}, {1, 2}, {2, 0}, {2, 1}},
             {{0, 0}, {1, 0}, {1, 1}, {2, 1}},
-    }, new int[]{0, 3}, 5, new BlockColor(0, 255, 1)),
+    }, new int[]{0, 3}, 5, new BlockColor(
+            Color.rgb(255, 165, 0),
+            Color.rgb(128, 0, 128),
+            Color.rgb(255, 165,0))
+    ),
 
     TBlock(new int[][][]{
             {{0, 1}, {1, 0}, {1, 1}, {1, 2}},
             {{0, 1}, {1, 1}, {1, 2}, {2, 1}},
             {{1, 0}, {1, 1}, {1, 2}, {2, 1}},
             {{0, 1}, {1, 0}, {1, 1}, {2, 1}},
-    }, new int[]{0, 3}, 6, new BlockColor(170, 0, 255)),
+    }, new int[]{0, 3}, 6, new BlockColor(
+            Color.rgb(135, 206, 235),
+            Color.rgb(173, 216, 230),
+            Color.rgb(255, 255, 224))
+    ),
 
     ZBlock(new int[][][]{
             {{0, 0}, {0, 1}, {1, 1}, {1, 2}},
             {{0, 2}, {1, 1}, {1, 2}, {2, 1}},
             {{1, 0}, {1, 1}, {2, 1}, {2, 2}},
             {{0, 1}, {1, 0}, {1, 1}, {2, 0}},
-    }, new int[]{0, 3}, 7, new BlockColor(254,0, 0));
+    }, new int[]{0, 3}, 7, new BlockColor(
+            Color.rgb(128, 0, 128),
+            Color.rgb(255, 200, 100),
+            Color.rgb(192, 192, 192))
+    );
 
     Block(int[][][] positions, int[] offset, int id, BlockColor blockColor) {
         int row = positions.length;
@@ -56,7 +90,12 @@ public enum Block {
         blockId = id;
         cells = new BlockPosition[row][];
         startOffset = new BlockPosition(offset[0], offset[1]);
-        this.blockColor = blockColor;
+        ConfigRepositoryImpl configRepository = ConfigRepositoryImpl.getInstance();
+        Map<String, Object> configs = configRepository.getConfig(0);
+        String colorMode = (String) configs.get("mode");
+        if (Objects.equals(colorMode, "Red-green")) {
+            this.blockColor = blockColor;
+        }
 
         for (int r = 0; r < row; r++) {
             int col = positions[r].length;
@@ -67,10 +106,11 @@ public enum Block {
         }
     }
 
+
     public final int blockId;
     public final BlockPosition[][] cells;
     public final BlockPosition startOffset;
-    public final BlockColor blockColor;
+    public BlockColor blockColor;
 
     public BlockPosition[] shape(int rotate) {
         return cells[rotate % cells.length];
--- app/src/main/java/org/se13/game/block/BlockColor.java
+++ app/src/main/java/org/se13/game/block/BlockColor.java
@@ -1,22 +1,32 @@
 package org.se13.game.block;
 
 import javafx.scene.paint.Color;
+import org.se13.sqlite.config.ConfigRepositoryImpl;
+
+import java.util.Map;
+import java.util.Objects;
 
 public class BlockColor {
-    BlockColor(int red, int green, int blue) {
-        this.defaultBlockColor = Color.rgb(red, green, blue);
-        this.isColorBlindMode = false;
+    public BlockColor(Color defaultColor, Color redGreenColorBlindColor, Color blueYellowColorBlindColor) {
+        this.defaultColor = defaultColor;
+        this.redGreenColorBlindColor = redGreenColorBlindColor;
+        this.blueYellowColorBlindColor = blueYellowColorBlindColor;
     }
 
     public Color getBlockColor() {
-        if (this.isColorBlindMode == true) {
-            return this.colorBlindModeBlockColor;
+        ConfigRepositoryImpl configRepository = ConfigRepositoryImpl.getInstance();
+        Map<String, Object> configs = configRepository.getConfig(0);
+        String colorMode = (String) configs.get("mode");
+        if (Objects.equals(colorMode, "Red-green")) {
+            return redGreenColorBlindColor;
+        } else if (Objects.equals(colorMode, "Blue-yellow")) {
+            return blueYellowColorBlindColor;
         } else {
-            return this.defaultBlockColor;
+            return defaultColor;
         }
     }
 
-    private Color defaultBlockColor;
-    private Color colorBlindModeBlockColor;
-    private boolean isColorBlindMode;
+    private final Color defaultColor;
+    private final Color redGreenColorBlindColor; // 적록색맹용 색상
+    private final Color blueYellowColorBlindColor; // 청황색맹용 색상
 }
