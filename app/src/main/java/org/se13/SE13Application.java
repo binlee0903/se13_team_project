e9ebafd0b61154dfce8e1459cf0a7b88bf4625e1
binlee0903 <egb3543@gmail.com>
2024-04-14 15:51:09 +0900

    fix: 사용자가 마우스로 크기조정할 수 없도록 변경

--- app/src/main/java/org/se13/SE13Application.java
+++ app/src/main/java/org/se13/SE13Application.java
@@ -14,6 +14,7 @@ public class SE13Application extends Application {
 
     @Override
     public void start(Stage stage) {
+        stage.setResizable(false);
         navController = new StackNavGraph(stage, ConfigRepositoryImpl.getInstance());
         navController.navigate(Screen.START);
     }
