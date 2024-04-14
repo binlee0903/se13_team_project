344167feea502926761fdbb3e364651a8e0a51a7
binlee0903 <egb3543@gmail.com>
2024-04-14 14:59:06 +0900

    feat: 난이도에 따라 떨어지는 정도 바꾸게 함

--- app/src/main/java/org/se13/game/timer/BlockFallingTimer.java
+++ app/src/main/java/org/se13/game/timer/BlockFallingTimer.java
@@ -1,11 +1,28 @@
 package org.se13.game.timer;
 
+import org.se13.game.rule.GameLevel;
+
 public class BlockFallingTimer extends Timer {
     public BlockFallingTimer(long startedTime) {
         super(startedTime);
     }
 
-    public void fasterBlockFallingTime() {
+    public void fasterBlockFallingTime(GameLevel difficulty) {
+
+        switch (difficulty) {
+            case EASY:
+                blockFallingTimeManipulator += (long) (BLOCK_FALLING_TIME_SUBTRACTOR * 0.8);
+                break;
+
+            case NORMAL:
+                blockFallingTimeManipulator += BLOCK_FALLING_TIME_SUBTRACTOR;
+                break;
+            case HARD:
+                blockFallingTimeManipulator += (long) (BLOCK_FALLING_TIME_SUBTRACTOR * 1.2);
+                break;
+            default:
+        }
+
         blockFallingTimeManipulator += BLOCK_FALLING_TIME_SUBTRACTOR;
     }
 
@@ -14,5 +31,5 @@ public class BlockFallingTimer extends Timer {
     }
 
     private final long BLOCK_FALLING_TIME_SUBTRACTOR = -100000000;
-    private long blockFallingTimeManipulator = 1000000000;
+    private long blockFallingTimeManipulator = 2000000000;
 }
