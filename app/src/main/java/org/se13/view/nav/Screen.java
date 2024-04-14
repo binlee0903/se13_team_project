b67f9fd2a971bbf3a68b1f4e765a6f248503ee0e
binlee0903 <egb3543@gmail.com>
2024-04-14 14:12:21 +0900

    feat: 게임 모드, 레벨 선택 추가

--- app/src/main/java/org/se13/view/nav/Screen.java
+++ app/src/main/java/org/se13/view/nav/Screen.java
@@ -2,6 +2,7 @@ package org.se13.view.nav;
 
 public enum Screen {
     START("StartScreen.fxml"),
+    LEVEL_SELECT("LevelSelectScreen.fxml"),
     TETRIS("TetrisScreen.fxml"),
     SETTING("SettingScreen.fxml"),
     RANKING("RankingScreen.fxml"),
