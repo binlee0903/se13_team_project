722779e1b5b7d8c11962d3a5cc1ddc77f24a3105
hyotaime <hyotaime@hyotaime.com>
2024-05-22 19:18:12 +0900

    feat: 직렬화, ObjectI/OStream 이용

--- app/src/main/java/org/se13/online/OnlineActionRepository.java
+++ app/src/main/java/org/se13/online/OnlineActionRepository.java
@@ -1,41 +1,36 @@
 package org.se13.online;
 
-import org.se13.game.action.TetrisAction;
 import org.se13.server.TetrisActionHandler;
 import org.se13.server.TetrisActionPacket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 
 public class OnlineActionRepository {
-    private static final Logger log = LoggerFactory.getLogger(OnlineActionRepository.class);
-    private int userId;
-    private TetrisActionHandler handler;
-    private Socket socket;
-    private BufferedReader in;
-
     public OnlineActionRepository(int userId, Socket socket, TetrisActionHandler handler) throws IOException {
         this.userId = userId;
         this.socket = socket;
         this.handler = handler;
-        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.in = new ObjectInputStream(socket.getInputStream());
     }
 
     public void read() {
-        // TODO: 직렬화 코드 필요
         while (true) {
             try {
-                int responseCode = in.read();
-                TetrisAction code = TetrisAction.fromCode(responseCode);
-                TetrisActionPacket packet = new TetrisActionPacket(userId, code);
+                TetrisActionPacket packet = (TetrisActionPacket) in.readObject();
                 handler.request(packet);
-            } catch (IOException e) {
+            } catch (IOException | ClassNotFoundException e) {
                 log.error(e.getMessage());
             }
         }
     }
+
+    private static final Logger log = LoggerFactory.getLogger(OnlineActionRepository.class);
+    private int userId;
+    private TetrisActionHandler handler;
+    private Socket socket;
+    private ObjectInputStream in;
 }
--- app/src/main/java/org/se13/online/OnlineEventRepository.java
+++ app/src/main/java/org/se13/online/OnlineEventRepository.java
@@ -6,33 +6,44 @@ import org.se13.view.tetris.TetrisEventRepository;
 import org.se13.view.tetris.TetrisGameEndData;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 
 public class OnlineEventRepository implements TetrisEventRepository {
-    private ExecutorService service;
-    private PrintWriter out;
-
     public OnlineEventRepository(Socket socket, ExecutorService service) throws IOException {
-        this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.out = new ObjectOutputStream(socket.getOutputStream());
         this.service = service;
     }
 
     @Override
     public void gameOver(TetrisGameEndData endData) {
-        // TODO: 직렬화 필요
-        service.execute(() -> out.write(""));
+        service.execute(() -> {
+            try {
+                out.writeObject(endData);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
     public void response(TetrisEvent event) {
-        // TODO: 직렬화 필요
-        service.execute(() -> out.write(""));
+        service.execute(() -> {
+            try {
+                out.writeObject(event);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
     public void subscribe(Subscriber<TetrisEvent> subscriber, Subscriber<TetrisGameEndData> isGameOver) {
         throw new UnsupportedOperationException("클라이언트가 서버의 OnlineEventRepository에 구독할 수 없습니다.");
     }
+
+    private ExecutorService service;
+    private ObjectOutputStream out;
 }
