08f9aa67fa317d26e76b5016813002b050fee6fd
hyotaime <hyotaime@hyotaime.com>
2024-05-27 12:41:16 +0900

    feat: Player별 Action, Event Repository 연결

--- app/src/main/java/org/se13/view/tetris/Player.java
+++ app/src/main/java/org/se13/view/tetris/Player.java
@@ -1,7 +1,14 @@
 package org.se13.view.tetris;
 
+import org.se13.online.ClientActionRepository;
+import org.se13.online.ClientEventRepository;
 import org.se13.server.*;
 import org.se13.sqlite.config.PlayerKeycode;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Player {
     public Player(int userId, PlayerKeycode playerKeycode, TetrisEventRepository repository) {
@@ -10,18 +17,29 @@ public class Player {
         this.eventRepository = repository;
     }
 
-    public Player(int userId) {
+    public Player(int userId, PlayerKeycode playerKeycode, Socket socket) throws IOException {
         this.userId = userId;
-        this.playerKeycode = new PlayerKeycode("", "", "", "", "", "", "");
-        this.eventRepository = new TetrisEventRepositoryImpl();
+        this.socket = socket;
+        this.service = Executors.newVirtualThreadPerTaskExecutor();
+        this.playerKeycode = playerKeycode;
+        this.eventRepository = new ClientEventRepository(socket, service);
         this.client = new TetrisClient(userId, eventRepository);
     }
 
     public void connectToServer(TetrisServer server) {
         this.server = server;
-
         this.actionHandler = server.connect(this.client);
         this.actionRepository = new TetrisActionRepositoryImpl(this.userId, this.actionHandler);
+    }
+
+    public void connectToOnlineServer(TetrisServer server) throws IOException {
+        this.server = server;
+        this.actionHandler = server.connect(this.client);
+        this.actionRepository = new ClientActionRepository(this.userId, this.socket);
+    }
+
+    public void connectOpponentToServer(TetrisServer server) {
+        this.server = server;
     }
 
     public PlayerKeycode getPlayerKeycode() {
@@ -41,6 +59,8 @@ public class Player {
     private TetrisActionRepository actionRepository;
     private TetrisClient client;
     private TetrisServer server;
+    private Socket socket;
+    private ExecutorService service;
     private TetrisActionHandler actionHandler;
     private PlayerKeycode playerKeycode;
 }
