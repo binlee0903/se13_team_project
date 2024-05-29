2415abcc942cd9f5020dda8da9c22b43288f7537
binlee0903 <egb3543@gmail.com>
2024-05-29 17:31:16 +0900

    feat: SettingScreenController test 추가

--- app/src/test/java/org/se13/view/controller/ControllerTest.java
+++ app/src/test/java/org/se13/view/controller/ControllerTest.java
@@ -1,17 +1,43 @@
 package org.se13.view.controller;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.se13.NavGraph;
 import org.se13.SE13Application;
+import org.se13.game.rule.GameMode;
 import org.se13.sqlite.config.ConfigRepository;
 import org.se13.sqlite.config.ConfigRepositoryImpl;
 import org.se13.view.base.BaseController;
+import org.se13.view.difficulty.LevelSelectScreenController;
 import org.se13.view.setting.SettingScreenController;
 
-public class ControllerTest {
+import java.util.Map;
+import java.util.logging.Level;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ControllerTest extends Application {
     public static NavGraph navController;
+    public static Stage stage;
+
+    @BeforeAll
+    public static void setUp() {
+        Thread thread = new Thread(() -> {
+            Application.launch(ControllerTest.class);
+        });
+
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ControllerTest.stage = stage;
+    }
 
     @Test
     @DisplayName("BaseController test")
@@ -19,5 +45,41 @@ public class ControllerTest {
         BaseController baseController = new BaseController();
         baseController.onCreate();
         baseController.onStart();
+    }
+
+    @Test
+    @DisplayName("LevelSelectScreenController test")
+    void testLevelSelectScreenController() {
+        LevelSelectScreenController levelSelectScreenController = new LevelSelectScreenController();
+        levelSelectScreenController.testInit();
+
+        levelSelectScreenController.initialize();
+
+        GameMode gameMode = levelSelectScreenController.setGameMode("default");
+        assertEquals(GameMode.DEFAULT, gameMode);
+
+        gameMode = levelSelectScreenController.setGameMode("item");
+        assertEquals(GameMode.ITEM, gameMode);
+
+        gameMode = levelSelectScreenController.setGameMode("timeLimit");
+        assertEquals(GameMode.TIME_LIMIT, gameMode);
+    }
+
+    @Test
+    @DisplayName("SettingScreenController test")
+    void testSettingScreenController() {
+        SettingScreenController settingScreenController = new SettingScreenController();
+        settingScreenController.testInit();
+
+        Map<String, String> keySettings = settingScreenController.getKeySettings();
+        settingScreenController.handleSettingClearButtonAction();
+
+        assertEquals("a", keySettings.get("keyLeft"));
+        assertEquals("d", keySettings.get("keyRight"));
+        assertEquals("s", keySettings.get("keyDown"));
+        assertEquals("w", keySettings.get("keyDrop"));
+        assertEquals("e", keySettings.get("keyRotate"));
+        assertEquals("p", keySettings.get("keyPause"));
+        assertEquals("q", keySettings.get("keyExit"));
     }
 }
