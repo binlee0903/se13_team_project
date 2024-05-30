9ab05a43ac5e92ad31c1ea1c125d656cb72f2f17
binlee0903 <egb3543@gmail.com>
2024-05-30 23:10:33 +0900

    feat: add more tests

--- app/src/test/java/org/se13/server/LocalBattleTetrisServerTest.java
+++ app/src/test/java/org/se13/server/LocalBattleTetrisServerTest.java
@@ -2,12 +2,9 @@ package org.se13.server;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.se13.game.action.TetrisAction;
 import org.se13.game.event.ServerErrorEvent;
-import org.se13.game.event.UpdateTetrisState;
 import org.se13.game.rule.GameLevel;
 import org.se13.game.rule.GameMode;
-import org.se13.game.tetris.TetrisGame;
 import org.se13.sqlite.config.ConfigRepositoryImpl;
 import org.se13.view.tetris.Player;
 
@@ -27,4 +24,31 @@ class LocalBattleTetrisServerTest {
         server.broadcast(new ServerErrorEvent("Hello"));
     }
 
+  @Test
+  @DisplayName("handleStartGame Test when session is null")
+  void testHandleStartGameNullSession() {
+      LocalBattleTetrisServer server = new LocalBattleTetrisServer(GameLevel.EASY, GameMode.DEFAULT);
+      server.handleStartGame(1);
+  }
+
+  @Test
+  @DisplayName("handleStartGame Test when player is already ready")
+  void testHandleStartGamePlayerAlreadyReady() {
+      LocalBattleTetrisServer server = new LocalBattleTetrisServer(GameLevel.EASY, GameMode.DEFAULT);
+      Player player = new Player(1, new ConfigRepositoryImpl(0).getPlayerKeyCode());
+      player.connectToServer(server);
+      server.handleStartGame(1);
+  }
+
+  @Test
+  @DisplayName("handleStartGame Test when all players are ready")
+  void testHandleStartGameAllPlayersReady() {
+      LocalBattleTetrisServer server = new LocalBattleTetrisServer(GameLevel.EASY, GameMode.DEFAULT);
+      Player player1 = new Player(1, new ConfigRepositoryImpl(0).getPlayerKeyCode());
+      Player player2 = new Player(2, new ConfigRepositoryImpl(1).getPlayerKeyCode());
+      player1.connectToServer(server);
+      player2.connectToServer(server);
+      server.handleStartGame(1);
+      server.handleStartGame(2);
+  }
 }
